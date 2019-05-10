aaffd1c91618899443fb115768985f55c3481f4a
erikhornstrom <erik.hornstrom@live.se>
2019-05-10 11:24:18 +0200

    small changes

--- 1.3/1.3.php
+++ 1.3/1.3.php
@@ -1,11 +1,11 @@
 <?php
-header( "Content-type: image/png" );
-$myImg = imagecreate( 240, 70);
-$background = imagecolorallocate( $myImg, 0, 255, 0 );
-$textColor = imagecolorallocate( $myImg, 255, 0, 255 );
-imagestring($myImg, 4, 30, 25, date("M,d,Y h:i:s A"), $textColor );
+header("Content-type: image/png");
+$myImg = imagecreate(240, 70);
+$background = imagecolorallocate($myImg, 0, 255, 0);
+$textColor = imagecolorallocate($myImg, 255, 0, 255);
+imagestring($myImg, 4, 30, 25, date("M,d,Y h:i:s A"), $textColor);
 imagepng($myImg);
-imagecolordeallocate( $textColor );
-imagecolordeallocate( $background );
-imagedestroy( $myImg );
+imagecolordeallocate($textColor);
+imagecolordeallocate($background);
+imagedestroy($myImg);
 ?>
